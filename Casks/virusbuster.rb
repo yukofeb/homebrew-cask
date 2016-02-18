368f911d2834db229409a80aec7542771f080e70
yukofeb <yuko.february@gmail.com>
2016-02-17 20:07:47 -0500

    add app, add elsif

--- Casks/virusbuster.rb
+++ Casks/virusbuster.rb
@@ -2,15 +2,14 @@ cask 'virusbuster' do
 	name 'virusbuster'
 	homepage 'http://www.trendmicro.co.jp'
 	license :commercial
-	link 'PackageSelector.app'
+	app 'PackageSelector.app'
 
 	if MacOS.release >= :mavericks
 		version '6.0.2020'
 		sha256 :no_check
 		url "http://files.trendmicro.com/jp/support/dl/itis60/Virus%20Buster%20for%20Mac-#{version}.dmg"
-	end
 
-	if MacOS.release == :mountain_lion
+	elsif MacOS.release == :mountain_lion
 		version '5.0.1178'
 		sha256 :no_check
 		url "http://files.trendmicro.com/jp/support/dl/itis50/Virus%20Buster%20for%20Mac-#{version}.dmg"
