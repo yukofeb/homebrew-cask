9451d36d635b70a0768ad550b6b503a5f7d1cda6
yukofeb <yuko.february@gmail.com>
2016-05-05 21:50:12 -0400

    remove needless file

--- Casks/nearlock.rb
+++
@@ -1,9 +0,0 @@
-cask 'nearlock' do
-	name 'nearlock'
-	homepage 'http://nearlock.me'
-	license :commercial
-	app 'PackageSelector.app'
-
-	sha256 :no_check
-	url "http://nearlock.me/downloads/nearlock.dmg"
-end
