6d47f90f4aa2baf49048f5126b8758114abb7bd1
yukofeb <yuko.february@gmail.com>
2016-05-05 21:21:48 -0400

    add nearlock

---
+++ Casks/nearlock.rb
@@ -0,0 +1,9 @@
+cask 'nearcask' do
+	name 'nearcask'
+	homepage 'http://nearlock.me'
+	license :commercial
+	app 'PackageSelector.app'
+
+	sha256 :no_check
+	url "http://nearlock.me/downloads/nearlock.dmg"
+end
